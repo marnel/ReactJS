571f098fe5985131879cce7b9e5d138429d18cc1
Michael Arnel <michael.arnel@gmail.com>
2017-02-04 07:15:36 -0500

    powered by css

--- Todos-Using-Redux/src/App.js
+++ Todos-Using-Redux/src/App.js
@@ -7,8 +7,8 @@ class App extends Component {
     return (
       <div className="App">
         <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <h1>TODOS using Redux!</h1>
+          <span className="powered-by">powered by:</span> <img src={logo} className="App-logo" alt="logo" />
         </div>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
